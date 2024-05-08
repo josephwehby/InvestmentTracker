4ec3314209bc53e8cd5bb4359b11231662dc4ad7
josephwehby <josephw31@hotmail.com>
2024-05-08 14:35:27 -0400

    removed old file

--- Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71914c1fe78476ddc61277c6b93c12af226b778")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad230fd7145e56a81b8cc9fab5596a503259609")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
