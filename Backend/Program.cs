b92c5fab89655cbb328a394618a4f646171513e3
josephwehby <josephw31@hotmail.com>
2024-06-04 12:12:59 -0500

    getting error with CORS. tried adding it in program.cs but not working

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,9 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<ITradeService, TradeService>();
 builder.Services.AddScoped<IPositionService, PositionService>();
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: "AllowReact",
+    policy =>
+      {
+        policy.WithOrigins("http://localhost:5174/")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+      });
+});
 
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.UseCors("AllowReact");
 app.Run();
--- Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712e4b95bffe444bc63108334747074f5b49aec5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b06a6de257971e3ff86898ce3af3df34c8e3084")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
