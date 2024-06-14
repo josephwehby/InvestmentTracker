3d71a4456555586fcede1172af30f841b585b074
josephwehby <josephw31@hotmail.com>
2024-06-14 11:26:05 -0500

    changed all the styling to a dark mode

--- Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07fab74b02d320ff091f5fddd5ed96e7d7d6069")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8496c5e367bf7263f98b8d0262fafe3ff9a3716")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import PortfolioValue from "../components/PortfolioValue";
 import Positions from "../components/Positions";
 import { UnrealizedGainsProvider } from "../contexts/UnrealizedGainsContext";
+import "../stylesheets/Home.css";
 
 function Home() {
   return (
