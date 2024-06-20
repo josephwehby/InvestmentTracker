15077e81e2e82393383a7e698d63a92cf5ce9e2d
josephwehby <josephw31@hotmail.com>
2024-06-20 13:36:26 -0500

    added fees section to trade form. also starting post request code

--- Frontend/src/components/OrderEntry.tsx
+++ Frontend/src/components/OrderEntry.tsx
@@ -6,14 +6,17 @@ function OrderEntry() {
   const [shares, setShares] = useState<number>(0);
   const [price, setPrice] = useState<number>(0);
   const [ordertype, setOrdertype] = useState<string>("buy");
+  const [fees, setFees] = useState<number>(0);
 
   function checkNumericalInput(e: ChangeEvent<HTMLInputElement>) {
     const { id, value } = e.target;    
     const clean_num = parseFloat(value);
     if(id === "shares") {
       setShares(clean_num);
-    } else {
+    } else if (id === "price"){
       setPrice(clean_num);
+    } else {
+      setFees(clean_num);
     }
   }
 
@@ -23,6 +26,16 @@ function OrderEntry() {
     setTicker(clean.toUpperCase());
   }
 
+  async function addTrade() {
+    const newTrade = {
+      'ticker': ticker,
+      'trade_type': ordertype,
+      'shares': shares,
+      'buy_price': price,
+      'fees': fees
+    };
+  }
+
   return (
     <>
       <div className="order-entry">
@@ -30,6 +43,7 @@ function OrderEntry() {
           <input type="text" id="ticker" placeholder="Ticker" value={ticker} onChange={checkStringInput} required />
           <input type="number" step=".1" id="shares" placeholder="Shares" value={shares} onChange={checkNumericalInput} required />
           <input type="number" step=".1" id="price" placeholder="Price" value={price} onChange={checkNumericalInput} required/>
+          <input type="number" step=".1" id="fees" placeholder="Fees" value={fees} onChange={checkNumericalInput} required/>
           <select id="order-type" value={ordertype} onChange={(e) => setOrdertype(e.target.value)}>
             <option value="buy">Buy</option>
             <option value="sell">Sell</option>
