70a8ab31545281075a717557fb5d53c0c58d6fb6
josephwehby <josephw31@hotmail.com>
2024-07-04 11:38:59 -0500

    wrote cron job to get closing day pnl. need to add the live stock quotes as this value is currently hard coded

--- CronJob/closing_pnl.py
+++ CronJob/closing_pnl.py
@@ -1,8 +1,5 @@
 import psycopg2
 
-
-CLOSING_PRICE = 100
-
 def connect():
     try:
         conn = psycopg2.connect(
@@ -24,8 +21,31 @@ def get_trades(cursor):
 
 def calculate_pnl(trades):
     # split up trades based on ticker
-    pnl = 0
+    trades = [list(t) for t in trades]
+    group = {}
+    
+    for sub in trades:
+        ticker = sub[2]
+        if ticker not in group:
+            group[ticker] = []
+        group[ticker].append(sub)
+    print(group)
+
+    total_pnl = 0
+    for ticker, positions in group.items():
+        ticker_pnl = 0
+        ticker_current_price = 100
+        for p in positions:
+            shares = p[3]
+            price = p[4]
+            pnl = (ticker_current_price - price) * shares
+            ticker_pnl += pnl
+        total_pnl += ticker_pnl 
+    
     return pnl
+
+def updateDatabase(cursor, pnl):
+    cursor.execute("INSERT INTO historic_pnl (pnl) VALUES (%s)", (pnl,))
 
 def main():
     print("[!] Calculating ending day pnl of all stock positions...")
@@ -36,8 +56,10 @@ def main():
         return None
     
     cursor = conn.cursor()
-    get_trades(cursor)
-
+    trades = get_trades(cursor)
+    pnl = calculate_pnl(trades)
+    updateDatabase(cursor, pnl)
+    conn.commit()
     conn.close()
 
 if __name__ == '__main__':
