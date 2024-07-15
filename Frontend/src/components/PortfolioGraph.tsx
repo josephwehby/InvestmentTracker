13288862ae2979558d2b4eea484da692c04c3a33
josephwehby <josephw31@hotmail.com>
2024-07-15 14:47:05 -0500

    added dates to dummy graph data

--- Frontend/src/components/PortfolioGraph.tsx
+++ Frontend/src/components/PortfolioGraph.tsx
@@ -1,24 +1,26 @@
-import { XAxis, YAxis, CartesianGrid, ResponsiveContainer, AreaChart, Area, Tooltip, TooltipProps } from 'recharts';
+import { XAxis, YAxis, CartesianGrid, ResponsiveContainer, AreaChart, Area, Tooltip, TooltipProps, angle } from 'recharts';
 import { ValueType, NameType } from 'recharts/types/component/DefaultTooltipContent';
 import "../stylesheets/PortfolioGraph.css";
 
 interface DataPoint {
-  day: number;
+  day: string;
   pnl: number;
 }
 
-const data: DataPoint[] = [
-  { day: 1, pnl: 20 },
-  { day: 2, pnl: 15 },
-  { day: 3, pnl: 5 },
-  { day: 4, pnl: 10 },
-  { day: 5, pnl: 20 },
-  { day: 6, pnl: 12 },
-  { day: 7, pnl: 3 },
-  { day: 8, pnl: 18 },
-  { day: 9, pnl: 10 },
-  { day: 10, pnl: 7 },
+const raw_data: DataPoint[] = [
+  { day: "5/1/2024", pnl: 20 },
+  { day: "5/2/2024", pnl: 15 },
+  { day: "5/3/2024", pnl: 5 },
+  { day: "5/4/2024", pnl: 10 },
+  { day: "5/5/2024", pnl: 20 },
+  { day: "5/6/2024", pnl: 12 },
+  { day: "5/7/2024", pnl: 3 },
+  { day: "5/8/2024", pnl: 18 },
+  { day: "5/9/2024", pnl: 10 },
+  { day: "5/10/2024", pnl: 7 },
 ];
+
+const data = raw_data.filter((_, index) => index % 2 === 0);
 
 function customeToolTip({active, payload, label} : TooltipProps<ValueType, NameType>) {
   if (active && payload && payload.length) {
@@ -43,7 +45,7 @@ function PortfolioGraph() {
         </linearGradient>
       </defs>
         <Area dataKey="pnl" fill="url(#color)" />
-        <XAxis dataKey="day" tickLine={false} tickFormatter={(tickItem) => tickItem%2 === 0 ? tickItem : ""}/>
+        <XAxis dataKey="day" />
         <YAxis dataKey="pnl" axisLine={false} tickLine={false} tickFormatter={ (n:number) => `$${n.toFixed(2)}`}/>
         <Tooltip content={customeToolTip}/>
         <CartesianGrid opacity={0.1} vertical={false} />
